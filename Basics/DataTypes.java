ec3ea19e612a2ef46d776ef5aa76a28de44ae416
RudyG252 <122180943+RudyG252@users.noreply.github.com>
2024-01-07 01:49:50 -0800

    Started
    1/3rd done with basics but probably needs improvement. Should be decent hands on tutorial.

---
+++ Basics/DataTypes.java
@@ -0,0 +1,16 @@
+    // if a line starts with // it doesn't affect the code
+    // You can declare a basic variable by filling in:
+    // <datatype> <name> = <value>;
+    // you can print a value by typing: System.out.println(<value>);
+public class DataTypes {
+    // Different Basic DataTypes: 
+    //  int: whole numbers (not fractions/decimals)
+    //  string: letters. make sure you put quotes around the value you want a string to be (ie "string")
+    //  double: decimal numbers
+    //  boolean: has two possible values: true or false
+    //  char: A single letter, denoted by apostrophes (ie: 'g', or 'e')
+    public static void main(String[] args) {
+        // now in the main method (the method you can run), create a variable
+        // of each basic data type and print it out.
+    }
+}
---
+++ Basics/Operators.java
@@ -0,0 +1,33 @@
+
+// you can modify a variable like this:
+// <variable> = <variable> <operator> <input>;
+// OR <variable> <operator>= <input>
+// for example:
+// x = x + 1;
+// OR x += 1;
+// multiple operators in the same line of code will follow standard PEMDAS
+// you can also convert double to ints (even in the middle of equations) by typing (int) <value>
+// you can do the same thing the other way around: (double) <value>
+public class Operators {
+    // an operator is a way to modify a variable or value
+    // Different types of operators:
+    // + : adds a value
+    // - : subtracts a value
+    // / : divides a value
+    // * : multiplies a value
+    // % : modulus: returns the remainder of the first number divided by the second number
+    //          for example: 3 % 3 = 0, 3 % 2 = 1, and 3 % 1 = 2
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 11;
+        int z = 18;
+        // create a new variable sol that is equal to the area of a triangle with these dimensions:
+        // the base is x + 5 long
+        // the height is z - y high
+        // print out the variable
+
+    }
+} 
+// running main should print out: 31.5
+// if your code is wrong attempt to find what's wrong and try again 2 more times
+// then go to someone for help
